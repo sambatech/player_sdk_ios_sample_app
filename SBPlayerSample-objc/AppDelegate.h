25f1119d81c15cbcd8fac0e185f5a082e0aef577
Leandro Zanol <lzanol@gmail.com>
2016-08-18 17:32:09 -0300

    added sample app (Obj-C)
    cleansed proj

---
+++ SBPlayerSample-objc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SBPlayerSample-objc
+//
+//  Created by Leandro Zanol on 8/18/16.
+//  Copyright © 2016 Samba Tech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
