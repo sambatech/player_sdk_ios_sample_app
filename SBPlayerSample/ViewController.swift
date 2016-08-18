aaea8cecb10e85e05dbfbf15c8e142148993961c
Leandro Zanol <lzanol@gmail.com>
2016-08-18 16:27:01 -0300

    added sample app (Swift)

---
+++ SBPlayerSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SBPlayerSample
+//
+//  Created by Leandro Zanol on 8/18/16.
+//  Copyright © 2016 Samba Tech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
